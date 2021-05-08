e5e29d24754be1bc9dc7a9c662111e266e965f2f
Flavia Ouyang <flavia.ouyang@mail.mcgill.ca>
2021-05-08 02:28:52 -0400

    selection sort done

--- WeekThree/selectionSort.c
+++ WeekThree/selectionSort.c
@@ -1,19 +1,33 @@
 #include<stdio.h>
 
+void swap(int*, int*);
 int main()
 {
     int array[7] = {15, 4, 3, 9, 6, 10, 2};
-    int smallest = array[0];
-    //find the smallest number
+    int j;
+    int min_index;
+
     for (int i = 0; i < 6; i++)
     {
-        for (int j = 0; j < 7; j++)
+        min_index = i;
+        for (j = i+1; j < 7; j++)
         {
-            if (array[j] < smallest){
-            smallest = array[i];
+            //find the smallest numbers
+            if (array[j] < array[min_index]){
+                min_index = j;
             }
         }
-        int temp = array[i];
-        array
+        swap(&array[min_index],&array[i]);
     }
+
+    for (int k = 0; k < 7; k++){
+        printf("%i\n", array[k]);
+    }
+}
+
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
 }
