0fb093b347837135aea1e28a19503d2605213297
Flavia Ouyang <flavia.ouyang@mail.mcgill.ca>
2021-05-08 02:07:24 -0400

    commit

--- WeekThree/selectionSort.c
+++ WeekThree/selectionSort.c
@@ -1,2 +1,19 @@
 #include<stdio.h>
 
+int main()
+{
+    int array[7] = {15, 4, 3, 9, 6, 10, 2};
+    int smallest = array[0];
+    //find the smallest number
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = 0; j < 7; j++)
+        {
+            if (array[j] < smallest){
+            smallest = array[i];
+            }
+        }
+        int temp = array[i];
+        array
+    }
+}
